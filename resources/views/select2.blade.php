87a7e075a0b389b402ce39559803d649c961386c
ferdousanam <ferdous.anam@gmail.com>
2022-10-01 16:46:57 +0600

    Code Seperated Using Layout

--- resources/views/select2.blade.php
+++ resources/views/select2.blade.php
@@ -1,32 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>{{ config('app.name') }}</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link href="https://cdn.jsdelivr.net/npm/select2@4.0.3/dist/css/select2.min.css" rel="stylesheet" />
-        <style>
-            .select2-container .select2-selection--single {
-                height: 40px;
-            }
-            .select2-container--default .select2-selection--single .select2-selection__rendered {
-                height: 100%;
-                display: flex;
-                align-items: center;
-            }
-            .select2-container--default .select2-selection--single {
-                 border-radius: 0;
-            }
-            .select2-container--default .select2-selection--single .select2-selection__arrow {
-                height: 100%;
-            }
-        </style>
-    </head>
-    <body class="bg-light">
+@push('styles')
+    <style>
+        .select2-container .select2-selection--single {
+            height: 40px;
+        }
+        .select2-container--default .select2-selection--single .select2-selection__rendered {
+            height: 100%;
+            display: flex;
+            align-items: center;
+        }
+        .select2-container--default .select2-selection--single {
+            border-radius: 0;
+        }
+        .select2-container--default .select2-selection--single .select2-selection__arrow {
+            height: 100%;
+        }
+    </style>
+@endpush
 
-    <div id="app" class="container">
+@section('content')
+    <div id="app">
         <div class="py-5 text-center">
             <h2>Select2: Dependent Dropdown</h2>
             <p class="lead">
@@ -72,24 +66,10 @@
                 </div>
             </div>
         </div>
-
-        <footer class="my-5 pt-5 text-muted text-center text-small">
-            <p class="mb-1">&copy; {{ date('Y') }} Ferdous Anam</p>
-            <ul class="list-inline">
-                <li class="list-inline-item"><a href="https://fb.me/ferdous.anam" target="_blank">Facebook</a></li>
-                <li class="list-inline-item"><a href="https://linkedin.com/in/ferdous-anam" target="_blank">LinkedIn</a></li>
-                <li class="list-inline-item"><a href="skype:ferdous_anam?add">Skype</a></li>
-            </ul>
-        </footer>
     </div>
-    </body>
+@endsection
 
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.10/dist/vue.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.3/dist/js/select2.min.js"></script>
-
+@push('scripts')
     <script>
         function formatState(state) {
             if (!state.id) {
@@ -163,4 +143,4 @@
             }
         });
     </script>
-</html>
+@endpush
